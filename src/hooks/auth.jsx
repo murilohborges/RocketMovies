308efa384e5277127e4146ea251918dbfb4c2647
murilo <mhenrique.mb@gmail.com>
2024-01-19 11:36:12 -0300

    setting localStorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from "../service/api";
 
 export const AuthContext = createContext({});
@@ -10,6 +10,10 @@ function AuthProvider({ children }) {
     try {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
+
+      localStorage.setItem("@rocketmovies:user", JSON.stringify(user));
+      localStorage.setItem("@rocketmovies:token", token);
+
       api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token });
 
@@ -22,6 +26,20 @@ function AuthProvider({ children }) {
     }
 
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketmovies:token");
+    const user = localStorage.getItem("@rocketmovies:user");
+
+    if(token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  })
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }} >
