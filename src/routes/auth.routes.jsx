e87f1d36a8fb474ef011488c6a54f229978af451
murilo <mhenrique.mb@gmail.com>
2024-01-25 10:43:41 -0300

    correction unavailable routes

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,13 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { SignIn } from '../Pages/SignIn';
 import { SignUp } from '../Pages/SignUp';
 
 export function AuthRoutes(){
+  const user = localStorage.getItem("@rocketmovies:user");
+
   return(
     <Routes>
       <Route path='/' element={<SignIn/>} />
       <Route path='/register' element={<SignUp/>} />
+      {!user && <Route path='*' element={<Navigate to='/'/>}/>}
     </Routes>
   )
 }
